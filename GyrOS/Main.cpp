f64f2174362eba49a550699fcb1172c2d3fda5e1
Rzar8767 <33533895+Rzar8767@users.noreply.github.com>
2017-12-27 00:54:03 +0100

    Revert "poczatki dysku"
    This reverts commit f4b7b5e1280961da7c1175e67c7554a1617de574.

--- GyrOS/Main.cpp
+++ GyrOS/Main.cpp
@@ -1,15 +1,3 @@
-#include "Dysk/Filesystem.h"
-#include <iostream>
-
-
 int main()
 {
-	Filesystem file;
-	file.createFile("Seba");
-	file.createFile("sonsiad");
-	file.deleteFile("Seba");
-
-	std::cout << file.print_directory() << std::endl;
-	getchar();
-	return 0;
 }
