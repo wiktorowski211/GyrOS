3ac68a9c81686a177eca18abc4224026161e5989
rav97 <35076475+rav97@users.noreply.github.com>
2018-01-04 17:36:58 +0100

    Update help.bat

--- GyrOS/help.bat
+++ GyrOS/help.bat
@@ -13,5 +13,5 @@ echo START		Uruchamia proces o wskazanych parametrach.
 echo TASKKILL	Zamyka wskazany proces.
 echo TASKLIST	Wyswietla liste procesow.
 echo REG		Umozliwia edycje rejestru systemowego.
-echo GO		Wywoluje obsluge pojedynczego rozkazu asemblerowego.
-echo CHECK    Wyswietla biezacy stan systemu.
+echo GO		Wywoluje kolejny 'krok' systemu (jeden rozkaz asemblerowy).
+echo CHECK		Wyswietla biezacy stan systemu.
