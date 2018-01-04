fd405d32a89b72d5a695ba993ed63caf74f9775c
rav97 <35076475+rav97@users.noreply.github.com>
2018-01-04 18:41:02 +0100

    Update Shell.h

--- GyrOS/Shell.h
+++ GyrOS/Shell.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -12,21 +13,23 @@ public:
 	map<string, int> rozkaz;
 	bool work;
 	Shell() {
-		rozkaz.insert(pair<string, int>("help", 1)); //wyœwietla dostêpne komendy
-		rozkaz.insert(pair<string, int>("cls", 2)); //czyœci konsole
+		rozkaz.insert(pair<string, int>("help", 1)); //wyÅ›wietla dostÄ™pne komendy
+		rozkaz.insert(pair<string, int>("cls", 2)); //czyÅ›ci konsole
 		rozkaz.insert(pair<string, int>("del", 3));//usuwa plik
-		rozkaz.insert(pair<string, int>("echo", 4));//przekazanie wartoœci na bufor lub do pliku
-		rozkaz.insert(pair<string, int>("exit", 5));//zamyka konsolê
-		rozkaz.insert(pair<string, int>("type", 6));//wyœwietla zawartoœæ pliku tekstowego
-		rozkaz.insert(pair<string, int>("rename", 7));//zmienia nazwê pliku
+		rozkaz.insert(pair<string, int>("echo", 4));//przekazanie wartoÅ›ci na bufor lub do pliku
+		rozkaz.insert(pair<string, int>("exit", 5));//zamyka konsolÄ™
+		rozkaz.insert(pair<string, int>("type", 6));//wyÅ›wietla zawartoÅ›Ä‡ pliku tekstowego
+		rozkaz.insert(pair<string, int>("rename", 7));//zmienia nazwÄ™ pliku
 		rozkaz.insert(pair<string, int>("start", 8));//uruchamia proces
-		rozkaz.insert(pair<string, int>("taskkill", 9));//koñczy proces
-		rozkaz.insert(pair<string, int>("tasklist", 10));//lista procesów
-		rozkaz.insert(pair<string, int>("go", 11));//wykonuje rozkaz asemblerowy
+		rozkaz.insert(pair<string, int>("taskkill", 9));//koÅ„czy proces
+		rozkaz.insert(pair<string, int>("tasklist", 10));//lista procesÃ³w
+		rozkaz.insert(pair<string, int>("go", 11));//wykonuje rozkaz asemblerowy - krok systemu
 		rozkaz.insert(pair<string, int>("check", 12));//pokazuje stan systemu 
 		work = true;
 	}
 	void wydziel_rozkaz(string &kom);
+	string begin(string &s);
+private:
 	void rozpoznaj_rozkaz(string s1, string s2);
 	bool echo_przekaz(string &s);
 	void help();
@@ -39,14 +42,14 @@ public:
 	void start(string &s);
 	void taskkill(string &s);
 	void tasklist();
-	void go(string &s);
+	void go();
 	void nadpisz(string &s, string &p);
 	void dopisz(string &s, string &p);
 	void czytaj_skrypt(string s);
 	void check();
 	string standard(string &s);
 	string namale(string &s);
-	string begin(string &s);
 	string end(string &s);
 	void kropki(string s);
+	string cudzy(string &s);
 };
