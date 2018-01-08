ade167c8b3420abe084cd8bd118a58ad554f1d55
Cerix7 <34888662+Cerix7@users.noreply.github.com>
2018-01-08 23:16:46 +0100

    Update ProcessManagement.h
    Nie ten plik xD

--- GyrOS/ProcessManagement.h
+++ GyrOS/ProcessManagement.h
@@ -1,32 +1,72 @@
 #pragma once
-#include <queue>
-#include "ProcessManagement.h"
-#include "ProcesorManager.h"
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
 
-class Semaphore
+
+struct Process
 {
 public:
-	int value;
-	queue<int> que;
-	Scheduler *ProcesorM;
-	ProcessManagement *ProcessM;
-	void Wait();
-	void Signal();
-	Semaphore(int k,Scheduler *procek, ProcessManagement *procesik)
-	{
-		value = k;
-		ProcessM = procesik;
-		procek = ProcesorM;
-	}
+	int PID;
+	int processState; //0=virgin, 1=ready, 2=running, 3=waiting, 4=terminated;
+	std::string name;
+
+	Process* parent; //procesy przedstawiaj¹ drzewo, gdzie rodzic jest procesem macierzystym
+	std::vector<Process*> children; //a dzieci jego procesami potomnymi
+
+	int regA;    // 4 wartoœci rejestrów procesora
+	int regB;
+	int regC;
+	int regD;
+
+	int programCounter = 0; //wskazuje nastêpn¹ instrukcjê do wykonania
+	int programPosition;
+
+	int GetPID();
+	int GetProcessState();
+
+	Process(int ID, Process* father, std::string processName, std::string txt); //konstruktor procesu;
+
 };
-struct SemaphoreFactory {
-	Scheduler *ProcesorM;
-	ProcessManagement *ProcessM;
-	Semaphore CreateSemaphore(int k);
-	SemaphoreFactory(Scheduler *p, ProcessManagement *h)
-	{
-		ProcesorM = p;
-		ProcessM = h;
-	}
+
+
+struct ProcessManagement
+{
+public:
+	Process* init; //proces, który wytwarza siê przy startowaniu systemu
+
+	std::vector<Process*> readyProcesses;
+	//
+	//
+	//
+	//
+	// tu dodaæ marcina rzeczy
+	//
+	//
+	//
+	//
+
+
+	int freeID = 0; //nastepny wolny numer ID do wykorzystania
+
+	ProcessManagement() {
+		init = new Process(freeID, nullptr, "init", NULL);
+		freeID++;
+	};
+
+	Process* FindProcess(int ID, Process* init); //funkcja przeszukuj¹ca drzewo procesów 
+
+	Process* FindProcess(std::string name, Process* init);
+
+	int FindFreeID(); //wybieranie wolnego ID
+
+	void AddProcess(std::string processName, std::string commands, int parentID = 0);
+
+	void KillProcess(std::string name);
+
+	void ChangeState(int ID, int newstate);
+
+	void PrintProcess(int ID);
+
+	void PrintAllProcesses();
 };
