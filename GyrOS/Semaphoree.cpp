39c6610467f15e41cc35bf428d81e498e4550165
Cerix7 <34888662+Cerix7@users.noreply.github.com>
2018-01-09 20:29:24 +0100

    Update Semaphoree.cpp

--- GyrOS/Semaphoree.cpp
+++ GyrOS/Semaphoree.cpp
@@ -2,15 +2,21 @@
 
 
 
-void Semaphore::Wait()
+bool Semaphore::Wait()
 {
 	if (value > 0)
-		value = value - 1;
+	{
+	value = value - 1;
+	return true;//wszedł
+	}
+		
 	else
 	{
 		string aktualny_proces = ProcessM->scheduler->GetProcess()->name;
 		que.push(aktualny_proces);
 		ProcessM->ChangeState(aktualny_proces, 3);
+		ProcessM->scheduler->GetProcess()->programCounter++;
+		return false;//nie wszedł
 	}
 }
 void Semaphore::Signal()
