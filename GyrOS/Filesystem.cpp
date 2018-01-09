a5593e0df0b35638201e8ce90f6ae74e21316196
Rzar8767 <33533895+Rzar8767@users.noreply.github.com>
2018-01-09 23:40:15 +0100

    bugfix print_directory
    i jeszcze jeden...

--- GyrOS/Filesystem.cpp
+++ GyrOS/Filesystem.cpp
@@ -446,7 +446,6 @@ bool Filesystem::readFile(const std::string& name, std::string& output) const
 void Filesystem::print_directory() const
 {
 	std::stringstream stream{};
-	std::string s;
 	stream << "Liczba wpisow: " << std::to_string(katalog.size()) + "\n"
 		<< std::setw(30) << std::left << "Nazwa:" 
 		<< std::setw(20) << std::left << "Numer i-wezla:"
@@ -463,9 +462,8 @@ void Filesystem::print_directory() const
 			<< std::setw(15) << std::left << std::to_string(wezly[x.second].size)
 			<< std::setw(18) << std::left << std::to_string(wezly[x.second].hardlinks)
 			<< '\n';
-	//	s += x.first + "\t\t\t" + std::to_string(x.second) + "\n";
 	}
-	printf_s("%s\n", stream.str());
+	printf_s("%s\n", stream.str().data());
 	//return stream.str();
 }
 
